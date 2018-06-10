eddde300aa9b8be64cdfbe99bdef16932b187915
Jon Betti <jonbetti@gmail.com>
2018-06-09 23:14:28 -0400

    Upgrade OpenVPN and produce hermetic iproute2

--- router_openvpn/vendor.sh
+++ router_openvpn/vendor.sh
@@ -3,9 +3,11 @@ set -e
 IPROUTE_VERSION=4.9.0
 (git clone git://git.kernel.org/pub/scm/linux/kernel/git/shemminger/iproute2.git \
   && cd iproute2 \
+  && git config advice.detachedHead false \
+  && echo "iproute '$(git tag -l |tail -1)' available" \
   && git checkout v$IPROUTE_VERSION \
-  && git archive --prefix iproute2/ HEAD . |gzip > ../iproute2.tar.gz \
+  && git archive --prefix iproute2/ HEAD . |gzip -n > ../iproute2.tar.gz \
   && cd .. \
   && rm -rf iproute2)
-OPENVPN_VERSION=2.4.4
+OPENVPN_VERSION=2.4.6
 wget -O openvpn.tar.gz "https://swupdate.openvpn.org/community/releases/openvpn-$OPENVPN_VERSION.tar.gz"
