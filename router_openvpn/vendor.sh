35addf2938e018c599c7776b3037d9275fd19efa
Jon Betti <jonbetti@gmail.com>
2018-06-10 21:24:25 -0400

    Downgrade OpenVPN a patch
    It didn't get along with PIA servers too well...

--- router_openvpn/vendor.sh
+++ router_openvpn/vendor.sh
@@ -9,5 +9,5 @@ IPROUTE_VERSION=4.9.0
   && git archive --prefix iproute2/ HEAD . |gzip -n > ../iproute2.tar.gz \
   && cd .. \
   && rm -rf iproute2)
-OPENVPN_VERSION=2.4.6
+OPENVPN_VERSION=2.4.5
 wget -O openvpn.tar.gz "https://swupdate.openvpn.org/community/releases/openvpn-$OPENVPN_VERSION.tar.gz"
